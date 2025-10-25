c88e679002c152000800c40c80f0db6a76164d55
allyIO <ohao@mail.uc.edu>
2025-10-24 21:40:33 -0400

    Adds DFS!!!

--- main.pl
+++ main.pl
@@ -37,72 +37,31 @@ run_all:-
 /* End of Adi's Code for Finding Path Cost */
 
 
-/* For the predicate search(N0, P, C),
-        there is a search path P from city N0 to 
-        Bucharest that costs C.
+/* 
+ *  solve_dfs(Start, Path, Cost)
+ *      Start: The first node of the path
+ *      Path: The path from Start node to bucharest
+ *      Cost: The cost of path Path 
+ */
+solve_dfs(Start, Path, Cost):-
+    dfs_search(Start, [], Path),
+    path_cost(Path, Cost).
 
-    Solution is when the last node is Bucharest.
-*/
+/* 
+ *  dfs_search(N0, Visited, Path)
+ *      N0: Start node
+ *      Visited: Nodes visited so far while generating path
+ *      Path: List of nodes from N0 to bucharest
+ *  Base case: There is only one node, and that node is bucharest
+ */
+dfs_search(N0, _, [N0]):-
+    N0 = bucharest.
 
-/* Base case is that the start city is Bucharest,
-    in which case the search path is simply [bucharest]
-    and  the cost is 0. */
-search(N0, P, C):-
-    N0 = bucharest,
-    P = [bucharest],
-    C = 0.
+dfs_search(N0, Visited, [N0|T]):-
+    connected(N0, N1, _),
+    \+ member(N1, Visited), % don't pursue N1 if it has been visited
+    dfs_search(N1, [N0|Visited], T).
 
-/* Recursive: search again on all nodes that have an 
-    edge with N0 until the base case of just Bucharest. */
-search(N0, [N0|T], C):-
-    T = [H|_],
-    edge(N0, H, C1), 
-    C = C1 + C2,
-    search(H, T, C2).
-
-
-
-
-
-
-/*************************************\
-    ALLY'S LAIR OF EMBARRASSMENT... 
-    THE CODE IS BAD NOW, DON'T LOOK
-\*************************************/
-
-dfs_search(N1, P, C):-
-    dfs_search(N1, P, [], C).
-
-dfs_search(N1, [N1], _, 0):-
-    N1 = 'Bucharest'.
-
-/* Q is queue of nodes from which to check. Q is maintained 
-    depending on the search strategy. */
-dfs_search(N0, [N0|T], [N1|QR], C):-
-    print([N0|T]),
-    /* Update queue based on new current node N1*/
-    dfs_enqueue(N1, QR, Q1),
-    /* The next node is N1 */
-    edge(N0, N1, CE),
-    /* The cost of the search is equal to the cost of the first 
-        edge + the cost of the search on the remaining tail */
-    C = CE + C0,
-    dfs_search(N1, T, Q1, C0).
-
-
-/* Append children of node N to the front of Q0 to get Q. */
-/* In DFS, the children of the current node are put at the 
-    front of the queue. */
-dfs_enqueue(N, Q0, Q):-
-    % edge(N, N1, _),
-    % if(N1 and not in Q0):
-    %     Q = [N1|Q0],
-    % else:
-    %     Q = Q0.
-    findall(City, edge(N, City), Children),
-    Q = [Children|Q0].
-
-% findall(city, edge(_, city), Children).
 
 
 /* Breadth-First Search Implementation */
